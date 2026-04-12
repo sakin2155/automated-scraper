2763416bcfb2030d323991189afc862a5fd70a1b
GitHub Action <action@github.com>
2026-04-12 18:30:57 +0000

    Automated daily scrape: 2026-04-12

---
+++ bulk_export_2026-04-12.sql
@@ -0,0 +1,309 @@
+-- Automated Bulk Export: 2026-04-12T18:25:49.927Z
+SET NAMES utf8mb4;
+
+-- Entry for pokemon-mega-evolution-special
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'pokemon-mega-evolution-special', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'pokemon-mega-evolution-special');
+
+-- Entry for devil-may-cry
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'devil-may-cry', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'devil-may-cry');
+
+-- Entry for relife
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'relife', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'relife');
+
+-- Entry for pokemon-horizons-the-series
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'pokemon-horizons-the-series', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'pokemon-horizons-the-series');
+
+-- Entry for the-new-scooby-doo-movies
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'the-new-scooby-doo-movies', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'the-new-scooby-doo-movies');
+
+-- Entry for attack-on-titan-muse-dub
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'attack-on-titan-muse-dub', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'attack-on-titan-muse-dub');
+
+-- Entry for fairy-tail
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'fairy-tail', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'fairy-tail');
+
+-- Entry for kunon-the-sorcerer-can-see
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'kunon-the-sorcerer-can-see', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'kunon-the-sorcerer-can-see');
+
+-- Entry for you-and-i-are-polar-opposites
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'you-and-i-are-polar-opposites', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'you-and-i-are-polar-opposites');
+
+-- Entry for nippon-sangoku-the-three-nations-of-the-crimson-sun
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'nippon-sangoku-the-three-nations-of-the-crimson-sun', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'nippon-sangoku-the-three-nations-of-the-crimson-sun');
+
+-- Entry for tojima-wants-to-be-a-kamen-rider
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'tojima-wants-to-be-a-kamen-rider', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'tojima-wants-to-be-a-kamen-rider');
+
+-- Entry for bofuri-i-dont-want-to-get-hurt-so-ill-max-out-my-defense
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'bofuri-i-dont-want-to-get-hurt-so-ill-max-out-my-defense', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'bofuri-i-dont-want-to-get-hurt-so-ill-max-out-my-defense');
+
+-- Entry for daemons-of-the-shadow-realm
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'daemons-of-the-shadow-realm', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'daemons-of-the-shadow-realm');
+
+-- Entry for trigun-stampede
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'trigun-stampede', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'trigun-stampede');
+
+-- Entry for a-misanthrope-teaches-a-class-for-demi-humans
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'a-misanthrope-teaches-a-class-for-demi-humans', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'a-misanthrope-teaches-a-class-for-demi-humans');
+
+-- Entry for frieren-beyond-journeys-end-hindi
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'frieren-beyond-journeys-end-hindi', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'frieren-beyond-journeys-end-hindi');
+
+-- Entry for the-ramparts-of-ice
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'the-ramparts-of-ice', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'the-ramparts-of-ice');
+
+-- Entry for naruto-shippuden-hindi-tamil-telugu
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'naruto-shippuden-hindi-tamil-telugu', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'naruto-shippuden-hindi-tamil-telugu');
+
+-- Entry for jujutsu-kaisen-hindi
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'jujutsu-kaisen-hindi', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'jujutsu-kaisen-hindi');
+
+-- Entry for naruto
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'naruto', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'naruto');
+
+-- Entry for solo-leveling-hindi
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'solo-leveling-hindi', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'solo-leveling-hindi');
+
+-- Entry for demon-slayer-kimetsu-no-yaiba
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'demon-slayer-kimetsu-no-yaiba', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'demon-slayer-kimetsu-no-yaiba');
+
+-- Entry for black-clover
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'black-clover', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'black-clover');
+
+-- Entry for my-hero-academia
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'my-hero-academia', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'my-hero-academia');
+
+-- Entry for the-daily-life-of-the-immortal-king-hindi
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'the-daily-life-of-the-immortal-king-hindi', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'the-daily-life-of-the-immortal-king-hindi');
+
+-- Entry for mashle-magic-and-muscles-hindi
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'mashle-magic-and-muscles-hindi', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'mashle-magic-and-muscles-hindi');
+
+-- Entry for wind-breaker-hindi
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'wind-breaker-hindi', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'wind-breaker-hindi');
+
+-- Entry for blue-lock-hindi
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'blue-lock-hindi', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'blue-lock-hindi');
+
+-- Entry for tower-of-god-hindi
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'tower-of-god-hindi', 'No description found.', '', 'series'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'tower-of-god-hindi');
+
+-- Entry for crayon-shin-chan-the-movie-super-hot-the-spicy-kasukabe-dancers
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'crayon-shin-chan-the-movie-super-hot-the-spicy-kasukabe-dancers', 'No description found.', '', 'movie'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'crayon-shin-chan-the-movie-super-hot-the-spicy-kasukabe-dancers');
+
+-- Entry for goat
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'goat', 'No description found.', '', 'movie'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'goat');
+
+-- Entry for pets-on-a-train
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'pets-on-a-train', 'No description found.', '', 'movie'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'pets-on-a-train');
+
+-- Entry for lego-marvel-avengers-loki-in-training
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'lego-marvel-avengers-loki-in-training', 'No description found.', '', 'movie'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'lego-marvel-avengers-loki-in-training');
+
+-- Entry for lego-marvel-super-heroes-black-panther-trouble-in-wakanda
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'lego-marvel-super-heroes-black-panther-trouble-in-wakanda', 'No description found.', '', 'movie'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'lego-marvel-super-heroes-black-panther-trouble-in-wakanda');
+
+-- Entry for lego-marvel-super-heroes-avengers-reassembled
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'lego-marvel-super-heroes-avengers-reassembled', 'No description found.', '', 'movie'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'lego-marvel-super-heroes-avengers-reassembled');
+
+-- Entry for lego-marvel-super-heroes-guardians-of-the-galaxy-the-thanos-threat
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'lego-marvel-super-heroes-guardians-of-the-galaxy-the-thanos-threat', 'No description found.', '', 'movie'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'lego-marvel-super-heroes-guardians-of-the-galaxy-the-thanos-threat');
+
+-- Entry for lego-marvel-spider-man-vexed-by-venom
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'lego-marvel-spider-man-vexed-by-venom', 'No description found.', '', 'movie'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'lego-marvel-spider-man-vexed-by-venom');
+
+-- Entry for lego-marvel-avengers-time-twisted
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'lego-marvel-avengers-time-twisted', 'No description found.', '', 'movie'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'lego-marvel-avengers-time-twisted');
+
+-- Entry for lego-dc-comics-super-heroes-batman-be-leaguered
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'lego-dc-comics-super-heroes-batman-be-leaguered', 'No description found.', '', 'movie'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'lego-dc-comics-super-heroes-batman-be-leaguered');
+
+-- Entry for zootopia-2
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'zootopia-2', 'No description found.', '', 'movie'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'zootopia-2');
+
+-- Entry for the-tigers-apprentice
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'the-tigers-apprentice', 'No description found.', '', 'movie'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'the-tigers-apprentice');
+
+-- Entry for solo-leveling-reawakening
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'solo-leveling-reawakening', 'No description found.', '', 'movie'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'solo-leveling-reawakening');
+
+-- Entry for jujutsu-kaisen-0
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'jujutsu-kaisen-0', 'No description found.', '', 'movie'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'jujutsu-kaisen-0');
+
+-- Entry for demon-slayer-kimetsu-no-yaiba-infinity-castle
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'demon-slayer-kimetsu-no-yaiba-infinity-castle', 'No description found.', '', 'movie'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'demon-slayer-kimetsu-no-yaiba-infinity-castle');
+
+-- Entry for your-name-hin
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'your-name-hin', 'No description found.', '', 'movie'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'your-name-hin');
+
+-- Entry for crayon-shin-chan-the-movie-our-dinosaur-diary
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'crayon-shin-chan-the-movie-our-dinosaur-diary', 'No description found.', '', 'movie'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'crayon-shin-chan-the-movie-our-dinosaur-diary');
+
+-- Entry for i-want-to-eat-your-pancreas
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'i-want-to-eat-your-pancreas', 'No description found.', '', 'movie'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'i-want-to-eat-your-pancreas');
+
+-- Entry for black-clover-sword-of-the-wizard-king
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'black-clover-sword-of-the-wizard-king', 'No description found.', '', 'movie'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'black-clover-sword-of-the-wizard-king');
+
+-- Entry for one-piece-the-movie
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'one-piece-the-movie', 'No description found.', '', 'movie'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'one-piece-the-movie');
+
+-- Entry for one-piece-clockwork-island-adventure
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'one-piece-clockwork-island-adventure', 'No description found.', '', 'movie'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'one-piece-clockwork-island-adventure');
+
+-- Entry for naruto-the-movie-ninja-clash-in-the-land-of-snow
+INSERT IGNORE INTO anime (title, description, poster_url, type) 
+SELECT 'naruto-the-movie-ninja-clash-in-the-land-of-snow', 'No description found.', '', 'movie'
+FROM (SELECT 1) AS tmp
+WHERE NOT EXISTS (SELECT 1 FROM anime WHERE title = 'naruto-the-movie-ninja-clash-in-the-land-of-snow');
+
